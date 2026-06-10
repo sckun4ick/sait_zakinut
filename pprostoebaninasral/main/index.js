2d03f88b1e239f848c9503afa3177b12834ced0b
sckun4ick <salexander0321@gmail.com>
2026-06-10 12:53:52 +0200

    Add files via upload

---
+++ pprostoebaninasral/main/index.js
@@ -0,0 +1,20 @@
+let catalog = document.getElementById('katalogoff');
+let catalog_butt = document.getElementById('catalog');
+let catalog_stat = false;
+
+let catalogImages = catalog_butt.querySelectorAll('img.list');
+
+catalog_butt.addEventListener('click', () => {
+    
+    catalog_stat = !catalog_stat;
+
+    if (catalog_stat) {
+        catalog.style.display = "flex";
+    } else {
+        catalog.style.display = "none";
+    }
+
+    catalogImages.forEach(img => {
+        img.classList.toggle('active');
+    });
+});
